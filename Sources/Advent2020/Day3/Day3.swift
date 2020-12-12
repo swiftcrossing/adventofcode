0c9ab9ae2d0aeba5681dd615db1fc08e05cbea5c
Nolan Warner <nolan.warner@accenture.com>
2020-12-12 20:04:22 +0900

    Solve day 3

--- Sources/Advent2020/Day3/Day3.swift
+++ Sources/Advent2020/Day3/Day3.swift
@@ -6,12 +6,22 @@ struct Day3: Solution {
   func solve1(for input: String, verbose: Bool) throws -> String {
     let parsed = try Day3Parser.parse(input)
     parsed.printContents(verbose: verbose)
-    return String(parsed.count)
+    let treeCount = parsed.treeCount(right: 3, down: 1)
+    return String(treeCount)
   }
 
   func solve2(for input: String, verbose: Bool) throws -> String {
     let parsed = try Day3Parser.parse(input)
     parsed.printContents(verbose: verbose)
-    return String(parsed.count * 2)
+    let treeCounts = [
+      parsed.treeCount(right: 1, down: 1),
+      parsed.treeCount(right: 3, down: 1),
+      parsed.treeCount(right: 5, down: 1),
+      parsed.treeCount(right: 7, down: 1),
+      parsed.treeCount(right: 1, down: 2),
+    ]
+    treeCounts.printContents(verbose: verbose)
+    let treeProduct = treeCounts.reduce(1, *)
+    return String(treeProduct)
   }
 }
